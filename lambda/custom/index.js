619ab0ef6a750055997b8ce755aadaf407ee4985
Radu Lucut <lucutradu@gmail.com>
2018-07-04 11:58:46 +0300

    Add fallback intent

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -12,10 +12,11 @@ const HELP_MESSAGE = `You can say:
     you can say exit... What can I help you with?`;
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
+const FALLBACK_MESSAGE = "The Ryu Concierge skill can't help with that";
 
 const handlers = {
     'LaunchRequest': function () {
-        const speechOutput = 'Hi! How can Ryu help you today?';
+        const speechOutput = 'Hi! How can Ryu Concierge help you today?';
 
         this.response.speak(speechOutput);
         this.emit(':responseReady');
@@ -35,6 +36,10 @@ const handlers = {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     },
+    'AMAZON.FallbackIntent': function () {
+        this.response.speak(FALLBACK_MESSAGE);
+        this.emit(':responseReady');
+    }
 };
 
 exports.handler = function (event, context, callback) {
